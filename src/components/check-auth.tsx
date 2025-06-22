443becab108d99fe9506cf2ce8d53319678a7967
Asaiy420 <kcaryan05@gmail.com>
2025-06-22 11:24:06 +0545

    add: logic to middleware

--- src/components/check-auth.tsx
+++ src/components/check-auth.tsx
@@ -1,11 +1,35 @@
-import type { ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface CheckAuthProps {
-  protected: boolean;
   children: ReactNode;
+  protectRoute: boolean;
 }
 
-const CheckAuth = ({ children }: CheckAuthProps) => {
+const CheckAuth = ({ children, protectRoute }: CheckAuthProps) => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (protectRoute) {
+      if (!token) {
+        navigate("/login");
+      } else {
+        setLoading(false);
+      }
+    } else {
+      if (token) {
+        navigate("/");
+      } else {
+        setLoading(false);
+      }
+    }
+  }, [navigate, protectRoute]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return <>{children}</>;
 };
 
