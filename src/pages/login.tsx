51f8c83aeab579a4ba80131bb176d88ad17128a7
Asaiy420 <kcaryan05@gmail.com>
2025-06-22 19:20:32 +0545

    add: (feat) shadcn

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,52 @@
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  })
+
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLFormElement>) => {
+    setForm({...form, [e.target.name]: e.target.value})
+  }
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try{
+      const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      })
+
+      const data = await res.json();
+
+
+      if (res.ok){
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+        navigate("/");
+      }else{
+        alert("Login failed")
+      }
+
+    }catch(error){
+      alert("Error when logging in")
+      console.error(error)
+    }finally{
+      setLoading(false)
+    }
+  }
+
   return (
     <div>login</div>
   )
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,8 +1,110 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@radix-ui/react-checkbox";
+import { Label } from "@radix-ui/react-label";
+import type React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
-  return (
-    <div>signup</div>
-  )
-}
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
 
-export default SignUp
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLFormElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_SERVER_URL}/auth/signup`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        }
+      );
+
+      const data = await res.json();
+
+      if (res.ok) {
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+        navigate("/");
+      } else {
+        alert("Sign up failed");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Error when signing up");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="relative flex flex-col justify-center items-center min-h-screen overflow-hidden">
+      <div className="w-full m-auto lg:max-w-lg">
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl text-center">Sign in</CardTitle>
+            <CardDescription className="text-center">
+              Enter your email and password to login
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+            <div className="flex items-center space-x-2">
+              <Checkbox id="terms" />
+              <label
+                htmlFor="terms"
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                Remember me
+              </label>
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col">
+            <form onSubmit={handleSignUp}>
+              <Button className="w-full" type="submit" disabled={loading}>
+                SignUp
+              </Button>
+            </form>
+            <p className="mt-2 text-xs text-center text-gray-700">
+              {" "}
+              Don't have an account?{" "}
+              <span className=" text-blue-600 hover:underline">Sign up</span>
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
